a17d12654a2d37fff7ee48290495e7f48b9c4cf6
Diekololaoluwa <ogundeyi.dieko13@gmail.com>
2025-08-29 06:14:51 -0600

    feat(Validation): Added validation for my posts

---
+++ server/middleware/signupvalidation.js
@@ -0,0 +1,34 @@
+import { body } from "express-validator";
+
+const signupValidation = [
+    body('name')
+        .notEmpty()
+        .withMessage("Name is required")
+        .isLength({ min: 3, max: 20 })
+        .withMessage("name must be between 3 to 20 characters"),
+    body('email')
+        .notEmpty()
+        .withMessage("Email is required")
+        .isEmail()
+        .withMessage("write email in correct format"),
+    body('password')
+        .notEmpty()
+        .withMessage("password is required")
+        .isLength({ min: 6, max: 20 })
+        .withMessage("password must be between 6 to 20 characters")
+]
+
+const loginValidation = [
+    body('email')
+        .notEmpty()
+        .withMessage("Email is required")
+        .isEmail()
+        .withMessage("write email in correct format"),
+    body('password')
+        .notEmpty()
+        .withMessage("password is required")
+        .isLength({ min: 6, max: 20 })
+        .withMessage("password must be between 6 to 20 characters")
+]
+
+export default { signupValidation, loginValidation };
