cb15b4e7dd662f75e31714d92d703ba6821be71a
Diekololaoluwa <ogundeyi.dieko13@gmail.com>
2025-09-01 14:01:38 -0600

    corrected my import

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { PrismaClient } from "@prisma/client";
-import { auth } from "../middleware/auth";
+import auth  from "../middleware/auth";
 const prisma = new PrismaClient();
 const router = express.Router();
 
